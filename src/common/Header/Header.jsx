66c873cf134faa6b5a97ab8b5fd1b4d7375529b6
King Joseph I <110317270+KingJosephI@users.noreply.github.com>
2022-11-22 01:51:27 +0100

    feat: add active state to the active page link

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -1,36 +1,60 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import logo from '../../assets/shared/desktop/logo.svg';
 import './Header.scss';
 const Header = () => {
+  // const activeStyle = {
+  //   color: 'red',
+  // };
   return (
     <header className="header">
       <div className="header__logo">
-        <Link to="/">
+        <NavLink
+          to="/"
+          style={({ isActive }) => ({ color: isActive ? 'grey' : 'black' })}
+        >
           <img src={logo} alt="logo" />
-        </Link>
+        </NavLink>
       </div>
+
       <nav className="header__nav">
         <ul className="header__menu">
           <li>
-            <Link className="header__link" to="/stories">
+            <NavLink
+              className="header__link"
+              to="/stories"
+              style={({ isActive }) => ({ color: isActive ? 'grey' : 'black' })}
+            >
               Stories
-            </Link>
+            </NavLink>
           </li>
           <li>
-            <Link className="header__link" to="/features">
+            <NavLink
+              className="header__link"
+              to="/features"
+              style={({ isActive }) => ({ color: isActive ? 'grey' : 'black' })}
+            >
               Features
-            </Link>
+            </NavLink>
           </li>
           <li>
-            <Link className="header__link" to="/pricing">
+            <NavLink
+              className="header__link"
+              to="/pricing"
+              style={({ isActive }) => ({ color: isActive ? 'grey' : 'black' })}
+            >
               Pricing
-            </Link>
+            </NavLink>
           </li>
         </ul>
       </nav>
       <a className="header__invite-button" href="!#">
         Get an invite
       </a>
+
+      <button className="header__hamburger">
+        <span></span>
+        <span></span>
+      </button>
     </header>
   );
 };
