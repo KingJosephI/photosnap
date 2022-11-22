975eb082a677400f2a656c4b4cc17581a34298ab
King Joseph I <110317270+KingJosephI@users.noreply.github.com>
2022-11-22 01:02:45 +0100

    style: reset the default styles and create sass variables and functions

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -1,22 +1,36 @@
 import { Link } from 'react-router-dom';
 import logo from '../../assets/shared/desktop/logo.svg';
+import './Header.scss';
 const Header = () => {
   return (
-    <header>
-      <div>
-        <img src={logo} alt="logo" />
+    <header className="header">
+      <div className="header__logo">
+        <Link to="/">
+          <img src={logo} alt="logo" />
+        </Link>
       </div>
-      <nav>
-        <li>
-          <Link to="/stories">Stories</Link>
-        </li>
-        <li>
-          <Link to="/features">Features</Link>
-        </li>
-        <li>
-          <Link to="/pricing">Pricing</Link>
-        </li>
+      <nav className="header__nav">
+        <ul className="header__menu">
+          <li>
+            <Link className="header__link" to="/stories">
+              Stories
+            </Link>
+          </li>
+          <li>
+            <Link className="header__link" to="/features">
+              Features
+            </Link>
+          </li>
+          <li>
+            <Link className="header__link" to="/pricing">
+              Pricing
+            </Link>
+          </li>
+        </ul>
       </nav>
+      <a className="header__invite-button" href="!#">
+        Get an invite
+      </a>
     </header>
   );
 };
