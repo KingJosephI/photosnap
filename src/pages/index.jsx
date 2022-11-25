74e2e3f8041347ca861bf782cf30c0581ae36372
King Joseph I <110317270+KingJosephI@users.noreply.github.com>
2022-11-25 23:42:33 +0100

    fix: homapage fix

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,4 @@
-export { default as HomePage } from './HomePage/HomePage';
+export { default as Homepage } from './Homepage/Homepage';
 export { default as StoriesPage } from './StoriesPage/StoriesPage';
 export { default as FeaturesPage } from './FeaturesPage/FeaturesPage';
 export { default as PricingPage } from './PricingPage/Pricing';
