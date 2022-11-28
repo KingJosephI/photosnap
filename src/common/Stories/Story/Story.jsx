0e98f34ebb6a0611c62e262deb4395d52e60fa2d
King Joseph I <110317270+KingJosephI@users.noreply.github.com>
2022-11-28 15:17:36 +0100

    fix(Story): add prop types to inforce the right data types passed in

--- src/common/Stories/Story/Story.jsx
+++ src/common/Stories/Story/Story.jsx
@@ -1,8 +1,8 @@
+import PropTypes from 'prop-types';
 import whiteArrow from '../../../assets/shared/desktop/arrow-white.svg';
-
 import './Story.scss';
 
-const Story = ({ image, title = '', author = '', date = '' }) => {
+const Story = ({ image = '', title = '', author = '', date = '' }) => {
   return (
     <div className="card">
       <div className="card__image">
@@ -23,6 +23,13 @@ const Story = ({ image, title = '', author = '', date = '' }) => {
       </div>
     </div>
   );
+};
+
+Story.propTypes = {
+  image: PropTypes.string,
+  title: PropTypes.string,
+  author: PropTypes.string,
+  date: PropTypes.string,
 };
 
 export default Story;
